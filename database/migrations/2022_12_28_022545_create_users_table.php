b57889e6647fda8299c63ca17d50242fb008bce9
sioputra <sioputra4@gmail.com>
2023-01-04 14:40:27 +0700

    Update Migration

--- database/migrations/2022_12_28_022545_create_users_table.php
+++ database/migrations/2022_12_28_022545_create_users_table.php
@@ -1,8 +1,11 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 return new class extends Migration
 {
@@ -21,6 +24,17 @@ return new class extends Migration
             $table->string('role', 25)->default('user');
             $table->timestamps();
         });
+        // Insert some stuff
+        DB::table('users')->insert(
+            array(
+                'username' => 'admin',
+                'name' => 'Admin',
+                'password' => Hash::make('admin'),
+                'role' =>'admin',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            )
+        );
     }
 
     /**
