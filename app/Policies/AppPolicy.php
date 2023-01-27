133b89e550d28c0cc043a0f75e612f0c5671e1b1
sioputra <sioputra4@gmail.com>
2023-01-27 15:22:13 +0700

    Add Policy

---
+++ app/Policies/AppPolicy.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Config;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class AppPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any configs.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the config.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Config  $config
+     * @return mixed
+     */
+    public function view(User $user, Config $config)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create configs.
+     *
+     * @param  \App\Models\User  $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can update the config.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Config  $config
+     * @return mixed
+     */
+    public function update(User $user, Config $config)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the config.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Config  $config
+     * @return mixed
+     */
+    public function delete(User $user, Config $config)
+    {
+        //
+    }
+}
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,9 +2,15 @@
 
 namespace App\Policies;
 
-
+use App\Helpers\UserHelper;
+use App\Models\Permision;
+use App\Models\Role;
+use App\Models\Permission;
+use App\Models\Privilege;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Auth;
 
 class UserPolicy
 {
@@ -19,18 +25,9 @@ class UserPolicy
     public function viewAny(User $user)
     {
         //
-    }
-
-    /**
-     * Determine whether the user can view any users.
-     *
-     * @param  \App\Models\User  $user
-     * @return mixed
-     */
-    public function viewAll(User $user)
-    {
-
-        return 1;
+        $uh = new UserHelper();
+        return $uh->checkPermision($user->user_id, ['super-admin', 'view-all-user']) ? Response::allow()
+            : Response::deny('Forbiden', 403);
     }
     /**
      * Determine whether the user can view the user.
@@ -42,6 +39,9 @@ class UserPolicy
     public function view(User $user)
     {
         //
+        $uh = new UserHelper();
+        return $uh->checkPermision($user->user_id, ['super-admin', 'view-user']) ? Response::allow()
+        : Response::deny('Forbiden', 403);
     }
 
     /**
@@ -53,6 +53,9 @@ class UserPolicy
     public function create(User $user)
     {
         //
+        $uh = new UserHelper();
+        return $uh->checkPermision($user->user_id, ['super-admin', 'add-user']) ? Response::allow()
+        : Response::deny('Forbiden', 403);
     }
 
     /**
@@ -65,6 +68,9 @@ class UserPolicy
     public function update(User $user)
     {
         //
+        $uh = new UserHelper();
+        return $uh->checkPermision($user->user_id, ['super-admin', 'edit-user']) ? Response::allow()
+        : Response::deny('Forbiden', 403);
     }
 
     /**
@@ -77,5 +83,8 @@ class UserPolicy
     public function delete(User $user)
     {
         //
+        $uh = new UserHelper();
+        return $uh->checkPermision($user->user_id, ['super-admin', 'delete-user']) ? Response::allow()
+        : Response::deny('Forbiden', 403);
     }
 }
