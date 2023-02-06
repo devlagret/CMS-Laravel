b31aad8c843b993da5cab8f190064c98093795ba
sioputra <sioputra4@gmail.com>
2023-02-06 10:18:15 +0700

    Update Controller

--- app/Http/Controllers/PermisionController.php
+++ app/Http/Controllers/PermisionController.php
@@ -15,8 +15,10 @@ class PermisionController extends Controller
         if ($request->user()->cannot('viewAny', Permision::class)) {
             return response('Unauthorized', 401);
         }
-        $permision = Permision::orderBy('group')->get(['permision_id','label as name','group']);
-        return response()->json($permision);
+        $group = Permision::orderBy('group')->distinct()->get('group');
+        $per = array();
+        foreach($group as $v){ $per[$v->group]=Permision::where('group',$v->group)->get(['permision_id', 'label as name']); }
+        return response()->json($per);
     }
     public function view(Request $request,$id){
         if ($request->user()->cannot('viewAny', Permision::class)) {
@@ -31,8 +33,9 @@ class PermisionController extends Controller
             return response('Unauthorized', 401);
         }
         $role = Role::find($id);
+        if (!$role) {return response('Not Found', 404);}
         $privilege = Privilege::where('role_id',$id)->get('permision_id');
-        if ($privilege->isEmpty()) {return response('Role id Not Found', 404);}
+        if ($privilege->isEmpty()) {return response("Role didn't have any permision", 404);}
         $permision = Permision::whereIn('permision_id', $privilege)->get(['permision_id','label','group']);
         return response()->json(['role_id' => $role->role_id,'role_name'=>$role->name,'permision'=>$permision]);
     }
